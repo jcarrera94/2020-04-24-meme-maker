956e1558538d8da7ba3db3fd6545f8cf14161137
Juan Carrera <juankarrera@gmail.com>
2020-04-24 15:17:46 -0400

    Updates

---
+++ precache-manifest.51808578bbaffc23587400c1cefbdd6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e678b9b5e3a0d750cde7e8a19523992",
+    "url": "/2020-04-24-meme-maker/index.html"
+  },
+  {
+    "revision": "422becc48948fe26a42f",
+    "url": "/2020-04-24-meme-maker/static/css/main.2fb6ae19.chunk.css"
+  },
+  {
+    "revision": "a870d6de757aa99d6613",
+    "url": "/2020-04-24-meme-maker/static/js/2.07b24181.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-04-24-meme-maker/static/js/2.07b24181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422becc48948fe26a42f",
+    "url": "/2020-04-24-meme-maker/static/js/main.a5381f88.chunk.js"
+  },
+  {
+    "revision": "3b9e786a5d0f5db4de99",
+    "url": "/2020-04-24-meme-maker/static/js/runtime-main.0962680e.js"
+  }
+]);
